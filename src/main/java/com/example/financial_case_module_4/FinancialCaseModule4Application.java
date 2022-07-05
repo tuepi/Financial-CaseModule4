2f738ad07a80d6788bbf3b2ba8facb9fc17f006e
tuepi <trandinhtue140398@gmail.com>
2022-07-05 17:42:36 +0700

    Tạo Project

---
+++ src/main/java/com/example/financial_case_module_4/FinancialCaseModule4Application.java
@@ -0,0 +1,13 @@
+package com.example.financial_case_module_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialCaseModule4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialCaseModule4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/financial_case_module_4/FinancialCaseModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financial_case_module_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialCaseModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
