020bf72203847bd2cca8862d0a8b00853dcebb78
thien2k37 <xxk30a6as2@gmail.com>
2022-07-08 08:44:46 +0700

    sua login , logout

--- src/main/resources/js/login.js
+++ src/main/resources/js/login.js
@@ -1,79 +1,133 @@
-
-
-let token = ""
+let token = window.sessionStorage.getItem("TOKEN_KEY");
+let name = window.sessionStorage.getItem("NAME_KEY");
 function showLogin() {
-    console.log("aaaaaa")
-    // if (token == "") {
+    if (token == null) {
         $("#modalLoginForm").modal('show');
-    // }
+    }
 }
 
-function showLoginForm() {
-    document.getElementById("container").innerHTML =
-        `    <div class="main-page">
-
-        <div class="smooth login" id="login">
-            <h1 class="login__header header">Welcome back to the party club</h1>
-            <p class="login__byline">It's good to see you again, come in using your favourite social network</p>
-            <div class="social-media__container">
-    <span class="fa-stack fa-lg social-media__icon icon">
-    <i class="fas fa-circle fa-stack-2x" style="color: #48556D;"></i>
-    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-    </span>
-                <span class="fa-stack fa-lg social-media__icon icon">
-    <i class="fas fa-circle fa-stack-2x" style="color: #DF4D3B;"></i>
-    <i class="fab fa-google-plus-g fa-stack-1x fa-inverse"></i>
-    </span>
-                <span class="fa-stack fa-lg social-media__icon icon">
-    <i class="fas fa-circle fa-stack-2x" style="color: #48556D;"></i>
-    <i class="fab fa-linkedin-in fa-stack-1x fa-inverse"></i>
-    </span>
-            </div>
-            <fieldset class="form">
-                <legend class="form__legend">OR</legend>
-                <div action="" class="form__body form-login">
-                    <input class="form__input" type="email" placeholder="username">
-                    <input class="form__input" type="password" placeholder="password">
-                    <button class="btn" type="submit">Sign in</button>
-                </div>
-            </fieldset>
-        </div>
-
-        <div class="switch">
-            <div class="switch__text-container"  id="switch-text">
-                <h1 class="switch__header header">Not yet a member?</h1>
-                <p>Sign up and discover what we can do for you</p>
-            </div>
-            <button class="btn-white btn" id="switch-button">Sign up</button>
-        </div>
-
-        <div class="smooth signup hide-view" id="signup">
-            <h1 class="signup__header header">Create a new account</h1>
-            <p class="signup__byline">You can use your favourite social network</p>
-            <div class="social-media__container">
-    <span class="fa-stack fa-lg social-media__icon icon">
-    <i class="fas fa-circle fa-stack-2x" style="color: #48556D;"></i>
-    <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-    </span>
-                <span class="fa-stack fa-lg social-media__icon icon">
-    <i class="fas fa-circle fa-stack-2x" style="color: #DF4D3B;"></i>
-    <i class="fab fa-google-plus-g fa-stack-1x fa-inverse"></i>
-    </span>
-                <span class="fa-stack fa-lg social-media__icon icon">
-    <i class="fas fa-circle fa-stack-2x" style="color: #48556D;"></i>
-    <i class="fab fa-linkedin-in fa-stack-1x fa-inverse"></i>
-    </span>
-            </div>
-            <fieldset class="form">
-                <legend class="form__legend">OR</legend>
-                <div action="" class="form__body form-login">
-                    <input class="form__input" type="email" placeholder="username">
-                    <input class="form__input" type="password" placeholder="password">
-                    <input class="form__input" type="password" placeholder="confirm password">
-                    <button class="btn" type="submit">Sign up</button>
-                </div>
-            </fieldset>
-        </div>
-
-    </div>`
+function login() {
+    let logInForm = {};
+    logInForm.username = $('#username').val();
+    logInForm.password = $('#password').val();
+    $.ajax({
+        async: false,
+        url: 'http://localhost:8000/login',
+        method: 'POST',
+        data: JSON.stringify(logInForm),
+        contentType: 'application/json; charset=utf8',
+        success: function (data) {
+            console.log("vao day")
+            window.sessionStorage.removeItem('TOKEN_KEY');
+            window.sessionStorage.setItem('TOKEN_KEY', data.accessToken);
+            window.sessionStorage.removeItem('NAME_KEY');
+            window.sessionStorage.setItem('NAME_KEY', data.username);
+            window.sessionStorage.removeItem('AVATAR_KEY');
+            window.sessionStorage.setItem('ID_USER_KEY', data.id);
+            let notification = "Successful login."
+            document.getElementById("notification").innerHTML = notification
+            $('#modalLoginForm').modal('hide');
+            $("#myModal").modal('show');
+            setTimeout(function () {
+                $("#myModal").modal('hide');
+            }, 1000);
+            profile()
+        }
+    })
 }
+function register() {
+    let noUser={
+        message:"no"
+    }
+    let createSuccess={
+        message:"yes"
+    }
+    let signUpForm = {};
+    signUpForm.username = $('#user').val();
+    signUpForm.password = $('#pass').val();
+    signUpForm.confirmPassword = $('#confirmPassword').val();
+    console.log(signUpForm);
+    $.ajax({
+        url: 'http://localhost:8000/register',
+        method: 'POST',
+        data: JSON.stringify(signUpForm),
+        contentType: 'application/json; charset=utf8',
+        success: function (data) {
+            console.log('data ===', data);
+            if (signUpForm.confirmPassword === signUpForm.confirmPassword) {
+                if (JSON.stringify(data) == JSON.stringify(noUser)) {
+                    document.getElementById('status').innerHTML = 'The username is existed! Please try again!'
+                    document.getElementById("status").style.color = "red";
+                }
+            }else {
+                document.getElementById("")
+            }
+            if (JSON.stringify(data) == JSON.stringify(createSuccess)) {
+                document.getElementById('status').innerHTML = 'Create User Account Success!'
+                document.getElementById("status").style.color = "green";
+            }
+            $("#modalLoginForm").modal('show');
+        }
+    })
+}
+function logout() {
+    window.sessionStorage.clear();
+    window.sessionStorage.removeItem("TOKEN_KEY")
+    profile()
+    $("#modalLoginForm").modal('show');
+}
+function change_pass() {
+    let no={
+        message:"no"
+    }
+    let yes={
+        message:"yes"
+    }
+    let changePasswordForm={};
+    changePasswordForm.currentPassword=$('#currentPassword').val();
+    changePasswordForm.newPassword=$('#newPassword').val();
+    let changePassword=JSON.stringify(changePasswordForm);
+    console.log("changPASSform=",changePasswordForm)
+    $.ajax({
+        headers:{
+            Authorization: 'Bearer ' + token
+        },
+        url: 'http://localhost:8081/users/change-password',
+        method: 'PUT',
+        data: changePassword,
+        contentType: 'application/json; charset=utf8',
+        success: function (data) {
+            console.log('data ===', data);
+            if (JSON.stringify(data) == JSON.stringify(no)) {
+                console.log("failed")
+                document.getElementById('status').innerHTML = 'Old password is not correct! Please try again!'
+            }
+            if (JSON.stringify(data) == JSON.stringify(yes)) {console.log("success")
+                document.getElementById('status').innerHTML = "Change password successfully!"
+            }
+        }
+    })
+}
+
+
+function profile() {
+    if (token != null) {
+        document.getElementById("profile").innerHTML = `<a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                           aria-haspopup="true" aria-expanded="false"><i class="fa " > ${name} </i> </a>
+                        <div class="dropdown-menu dropdown-menu-right dropdown-cyan"
+                             aria-labelledby="navbarDropdownMenuLink-4" id="">
+                            <a class="dropdown-item" data-target="#modalLoginForm" onclick="logout()">Logout</a>
+                            <a class="dropdown-item" data-target="#modalRegisterForm" onclick="change_pass()">ChangePassword</a>
+                        </div>`;
+    } else {
+        document.getElementById("profile").innerHTML = ""
+    }
+}
+
+
+
+
+
+
+
+
